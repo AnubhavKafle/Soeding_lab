1ec944cc4f3f96c4e3cc13e8fb560b014d6952f5
Anubhav Kaphle <anubhavkaphle@gmail.com>
2017-12-20 18:18:08 +0100

    Update LASSO_Reverse_regression.cpp

--- LASSO_Reverse_regression.cpp
+++ LASSO_Reverse_regression.cpp
@@ -25,7 +25,7 @@ float likelihood_ratio_test(double* expr, vec beta,vec response,int sample,int n
 
 	for ( int s=0; s < sample; s++) {
 			float ss = pow(response[s] - mean,2);
-	    deflection = deflection + ss;
+	        deflection = deflection + ss;
 	 }
 
 	float variance = deflection/sample;
@@ -36,7 +36,7 @@ float likelihood_ratio_test(double* expr, vec beta,vec response,int sample,int n
 
 			for (int j=0; j<ngene; j++){
 
-				int remainder = beta[j]*100;
+				int remainder = beta[j]*1000;
 
 			if(remainder%10 != 0){
 
@@ -57,7 +57,7 @@ float likelihood_ratio_test(double* expr, vec beta,vec response,int sample,int n
 }
 
 
-DLLEXPORT int fit ( double* genotype, double* expression, int nsnps, int ngene, int nsample, double* LLR , float* Beta_indices){
+DLLEXPORT int fit ( double* genotype, double* expression, int nsnps, int ngene, int nsample, double* LLR , double* Beta_indices){
 
 	int i,j,k;
 	int pos;
@@ -74,8 +74,6 @@ DLLEXPORT int fit ( double* genotype, double* expression, int nsnps, int ngene, 
 		exit ( 0 );
 	}
 
-
-
 	for (i = 0 ; i < nsnps; i++) {
 	    // Get the x for regression (ndonor elements)
 	    for (j = 0; j < nsample; j++){
@@ -90,48 +88,21 @@ DLLEXPORT int fit ( double* genotype, double* expression, int nsnps, int ngene, 
 
 	    vec response = vec(x, nsample);
 
-	    vec beta = vec(ngene);
+	    //vec beta = vec(ngene);
+	    vec Beta = vec(Beta_indices+(i*ngene),ngene,false);
 
-	    LARS lasso(true, 0.4);
+	    LARS lasso(true, 0.02);
 
-	    lasso.Train(A,response, beta, false);
+	    lasso.Train(A,response, Beta, false);
 
-	    likeli = likelihood_ratio_test(expression,beta,response,nsample,ngene);
+	    likeli = likelihood_ratio_test(expression,Beta,response,nsample,ngene);
 
 	    LLR[i] = likeli; 
         
-        for (k=0;k<ngene;k++){
-
-        	Beta_indices[i*ngene+k] = beta[k];
-        }
-
 	}
 
 	free ( x );
-	//free ( y );
 	x = NULL;
-	//y = NULL;
 	return 0;
 
 }
-
-/*
-int main()
-{
-    
-   double n_rows[30] = {1,1,1,1,1,1,1,1,1,1,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};   // samples*genes  
-   double *pp = n_rows; 
-   mat A = mat(pp,10,3);
-   double responses[10] = {10,20,30,40,50,60,70,80,90,100}; // column vector of SNPs
-   double *qq = responses;
-   vec y = vec(qq,10);
-   vec beta = vec(3);
-   LARS lasso(true,0.5); // lambda needs to be choosen 
-   lasso.Train(A,y, beta, false);
-   vec ss = lasso.LambdaPath();
-   //vec parameters = lassop.Parameters();
-   //mat A = mat(n_rows,n_cols);
-   cout<<"Hello world!"<< A << endl;
-   
-}
-*/
